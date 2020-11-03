6d09af4798603f3b9bab266b40b5a6e8e0afd617
tolunchuk <playgametolun@mail.ru>
2020-11-03 19:01:55 +1000

    123
    123

--- script.js
+++
@@ -1,8 +0,0 @@
-	//var canvas = document.getElementById('c1');
-//var ctx = canvas.getContext('2d');	
-	//document.querySelector(``).addEventListener(`click`,function() {
-		
-	//});
-	document.querySelector(`body`).onkeypress = function(event) {
-		
-	}
